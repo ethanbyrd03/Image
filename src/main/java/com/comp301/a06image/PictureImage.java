40cfa9d213761bcec9f95068a84a45a91621b7d8
Ethan Byrd <ethbyr@ad.unc.edu>
2023-10-25 12:58:28 -0400

    Novice

--- src/main/java/com/comp301/a06image/PictureImage.java
+++ src/main/java/com/comp301/a06image/PictureImage.java
@@ -1,15 +1,10 @@
 package com.comp301.a06image;
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Iterator;
 
 public class PictureImage implements Image {
 
@@ -17,12 +12,8 @@ public class PictureImage implements Image {
 
   public PictureImage(String pathname) throws IOException {
     BufferedImage image;
-    Path picPath = Paths.get(pathname);
-    ImageInputStream is = ImageIO.createImageInputStream(picPath);
-    Iterator<ImageReader> iter = ImageIO.getImageReaders(is);
-    ImageReader imageReader = (ImageReader) iter.next();
-    imageReader.setInput(is);
-    image = imageReader.read(0);
+    File img = new File(pathname);
+    image = ImageIO.read(img);
     this.picture = image;
   }
 
