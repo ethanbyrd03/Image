fcfbfea5eeb55a9b554b5a263f8cb7e17bf6a082
Ethan Byrd <ethbyr@ad.unc.edu>
2023-10-25 14:58:30 -0400

    Jedi Border

---
+++ src/main/java/com/comp301/a06image/BorderDecorator.java
@@ -0,0 +1,49 @@
+package com.comp301.a06image;
+
+import java.awt.*;
+
+public class BorderDecorator implements Image {
+
+  private Image _image;
+  private int _thickness;
+  private Color _borderColor;
+
+  public BorderDecorator(Image image, int thiccness, Color borderColor) {
+    if (image == null) {
+      throw new IllegalArgumentException();
+    }
+    if (thiccness < 0) {
+      throw new IllegalArgumentException();
+    }
+    this._image = image;
+    this._thickness = thiccness;
+    this._borderColor = borderColor;
+  }
+
+  @Override
+  public Color getPixelColor(int x, int y) {
+    if (x > this._image.getWidth()
+        && x < this._image.getWidth() + this._thickness
+        && y > this._image.getHeight()
+        && y < this._image.getHeight() + this._thickness) {
+      return this._borderColor;
+    } else {
+      return this._image.getPixelColor(x, y);
+    }
+  }
+
+  @Override
+  public int getWidth() {
+    return this._image.getWidth() + 2 * this._thickness;
+  }
+
+  @Override
+  public int getHeight() {
+    return this._image.getWidth() + 2 * this._thickness;
+  }
+
+  @Override
+  public int getNumLayers() {
+    return this._image.getNumLayers() + 1;
+  }
+}
