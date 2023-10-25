7a742e0c67e101b4be22c5fed836373a392c4032
Ethan Byrd <ethbyr@ad.unc.edu>
2023-10-25 13:30:30 -0400

    Adept Square

---
+++ src/main/java/com/comp301/a06image/SquareDecorator.java
@@ -0,0 +1,60 @@
+package com.comp301.a06image;
+
+import java.awt.*;
+
+public class SquareDecorator implements Image {
+
+  private Image _image;
+  private int _squareX;
+  private int _squareY;
+  private int _squareSize;
+  private Color _color;
+
+  public SquareDecorator(Image image, int squareX, int squareY, int squareSize, Color color) {
+    if (image == null) {
+      throw new IllegalArgumentException();
+    }
+    if (squareX < 0 || squareX >= image.getWidth()) {
+      throw new IllegalArgumentException();
+    }
+    if (squareY < 0 || squareY >= image.getHeight()) {
+      throw new IllegalArgumentException();
+    }
+    if (squareSize < 0) {
+      throw new IllegalArgumentException();
+    }
+    if (color == null) {
+      throw new IllegalArgumentException();
+    }
+    this._image = image;
+    this._squareX = squareX;
+    this._squareY = squareY;
+    this._squareSize = squareSize;
+    this._color = color;
+  }
+
+  @Override
+  public Color getPixelColor(int x, int y) {
+    if (x >= this._squareX && x <= (this._squareX + this._squareSize)) {
+      if (y >= this._squareY && y <= (this._squareY + this._squareSize)) {
+        return this._color;
+      }
+    }
+    return this._image.getPixelColor(x, y);
+  }
+
+  @Override
+  public int getWidth() {
+    return this._squareSize;
+  }
+
+  @Override
+  public int getHeight() {
+    return this._squareSize;
+  }
+
+  @Override
+  public int getNumLayers() {
+    return this._image.getNumLayers() + 1;
+  }
+}
